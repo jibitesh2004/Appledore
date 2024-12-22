6bc6c7de664640847de6cf43bd2d32746de527e6
SharonIV0X86 <anirudhlakhanpal123@gmail.com>
2024-12-22 17:11:38 +0530

    + improvements

--- include/GraphMatrix.h
+++ include/GraphMatrix.h
@@ -8,7 +8,7 @@
 
 namespace Appledore
 {
-    // Tag classes
+    // Tag structures
     struct DirectedG
     {
     };
@@ -18,32 +18,59 @@ namespace Appledore
     struct UnweightedG
     {
     };
+    class GraphVertex
+    {
+    public:
+        size_t id;
 
-    // Edge information struct
+        static size_t nextId;
+
+        GraphVertex()
+        {
+            id = nextId++;
+        }
+        bool operator<(const GraphVertex &other) const
+        {
+            return id < other.id;
+        }
+    };
+    size_t Appledore::GraphVertex::nextId = 1;
     template <typename EdgeType>
     struct EdgeInfo
     {
         EdgeType value;
 
-        EdgeInfo() : value() {}
+        // EdgeInfo() : value() {}
         EdgeInfo(const EdgeType &value) : value(value) {}
     };
 
     // GraphMatrix class template
-    template <typename VertexType, typename EdgeType = UnweightedG, typename Direction = UndirectedG>
+    template <typename VertexType, typename EdgeType, typename Direction>
     class GraphMatrix
     {
     public:
-        GraphMatrix() : isWeighted{!std::is_same_v<EdgeType, UnweightedG>}, isDirected{!std::is_same_v<EdgeType, UndirectedG>} {};
-        // Add vertex
-        void addVertex(const VertexType &vertex)
-        {
-            if (vertexToIndex.count(vertex))
-                return;
+        GraphMatrix()
+            : isWeighted(!std::is_same_v<EdgeType, UnweightedG>),
+              isDirected(std::is_same_v<Direction, DirectedG>) {}
 
-            size_t newIndex = numVertices++;
-            vertexToIndex[vertex] = newIndex;
-            indexToVertex.push_back(vertex);
+        template <typename... Vertices>
+        void addVertex(Vertices &&...vertices)
+        {
+            auto add_single_vertex = [this](const VertexType &vertex)
+            {
+                if (vertexToIndex.count(vertex))
+                    return;
+                // wont work.
+                //  if (!std::is_same_v<vertices, VertexType>)
+                //  {
+                //      throw std::invalid_argument("All arguments must be of type VertexType");
+                //  }
+                size_t newIndex = numVertices++;
+                vertexToIndex[vertex] = newIndex;
+                indexToVertex.push_back(vertex);
+            };
+
+            (add_single_vertex(std::forward<Vertices>(vertices)), ...);
 
             adjacencyMatrix.resize(numVertices * numVertices, std::nullopt);
         }
@@ -54,8 +81,8 @@ namespace Appledore
                 return false;
             }
 
-            size_t srcIndex = vertexToIndex.at(src);
-            size_t destIndex = vertexToIndex.at(dest);
+            size_t srcIndex = vertexToIndex[src];
+            size_t destIndex = vertexToIndex[dest];
 
             if (adjacencyMatrix[getIndex(srcIndex, destIndex)].has_value())
             {
@@ -66,27 +93,6 @@ namespace Appledore
                 return false;
             }
         }
-        void randomTest()
-        {
-            if (isDirected)
-            {
-                std::cout << "The graph is directed\n";
-            }
-            else
-            {
-                std::cout << "The graph is not directed\n";
-            }
-
-            if (isWeighted)
-            {
-                std::cout << "The graph is weighted\n";
-            }
-            else
-            {
-                std::cout << "The graph is not weighted\n";
-            }
-        }
-
         // for weighted.
         void addEdge(const VertexType &src, const VertexType &dest, const EdgeType &edge)
         {
@@ -131,10 +137,13 @@ namespace Appledore
             size_t srcIndex = vertexToIndex.at(src);
             size_t destIndex = vertexToIndex.at(dest);
 
+            std::cout << "Removing edge from src -> dest\n";
             adjacencyMatrix[getIndex(srcIndex, destIndex)] = std::nullopt;
 
-            if constexpr (std::is_same<Direction, UndirectedG>::value)
+            std::cout << "Flag: " << std::boolalpha << isDirected << "\n";
+            if (!isDirected)
             {
+                std::cout << "Removing edge from dest -> src\n";
                 adjacencyMatrix[getIndex(destIndex, srcIndex)] = std::nullopt;
             }
         }
@@ -156,14 +165,55 @@ namespace Appledore
         {
             return indexToVertex;
         }
+        const EdgeType &getEdge(VertexType &src, VertexType &dest) const
+        {
+            if (!vertexToIndex.count(src) || !vertexToIndex.count(dest))
+            {
+                throw std::invalid_argument("One of both vertices do not exist!");
+            }
+            size_t srcIndex = vertexToIndex.at(src);
+            size_t destIndex = vertexToIndex.at(dest);
+
+            auto &egdeValue = adjacencyMatrix[getIndex(srcIndex, destIndex)];
+
+            if (egdeValue.has_value())
+            {
+                return egdeValue.value().value;
+            }
+            else
+            {
+                throw std::runtime_error("No Edge exists between the given vertices.");
+            }
+        }
+
+        std::vector<std::tuple<VertexType, VertexType, EdgeType>> getAllEdges() const
+        {
+            std::vector<std::tuple<VertexType, VertexType, EdgeType>> edges;
+
+            for (size_t srcIndex = 0; srcIndex < numVertices; ++srcIndex)
+            {
+                for (size_t destIndex = 0; destIndex < numVertices; ++destIndex)
+                {
+                    const auto &edgeValue = adjacencyMatrix[getIndex(srcIndex, destIndex)];
+                    if (edgeValue.has_value())
+                    {
+                        edges.emplace_back(
+                            indexToVertex[srcIndex],
+                            indexToVertex[destIndex],
+                            edgeValue.value().value);
+                    }
+                }
+            }
+            return edges;
+        }
 
     private:
         std::map<VertexType, size_t> vertexToIndex;
         std::vector<VertexType> indexToVertex;
         std::vector<std::optional<EdgeInfo<EdgeType>>> adjacencyMatrix;
         size_t numVertices = 0;
-        bool isWeighted;
         bool isDirected;
+        bool isWeighted;
         inline size_t getIndex(size_t src, size_t dest) const
         {
             return src * numVertices + dest;
